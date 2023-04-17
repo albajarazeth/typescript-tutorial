a64d66da6b78aa13ea0151e2ce7a021428c4f045
Alba Garza <jarazethalba@gmail.com>
2023-04-16 23:13:03 -0600

    concepts

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -7,9 +7,13 @@ let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-type Person = {
+interface Person{
   name: string;
   age?: number;
+}
+
+interface Position extends Person{
+  profession: string;
 }
 
 let personOne: Person = {
@@ -18,10 +22,35 @@ let personOne: Person = {
 
 let lotsOfPeople: Person[];
 
+//let printOtherName: Function;
+let printOtherName: (name: string) => never;
+
+function printName(name: string){
+  console.log(name);
+}
+
+type X = {
+ a: string,
+ b: number
+} 
+
+type Y = X & {
+  c: string,
+  d: number
+};
+
+/*
+let y: Y = {
+  c: "William",
+  d: 45
+}
+*/
+
 function App() {
   return (
     <div className="App">
       <h1>main</h1>
+      <button onClick={()=>printName("Marie")}>CLICK ME</button>
     </div>
   );
 }
